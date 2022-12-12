005d6e9b2623b7f437736c79317d277fc837e3ac
Milton <milton@enviopack.com>
2022-12-12 15:34:42 -0300

    cambie el texto del componente App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hola soy Milton</h1>
+      <h2>Gera me hace laburar</h2>
     </div>
   );
 }
