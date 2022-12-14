abb8138941c1e9dcd40a5f11a22dc683381df352
Milton <milton@enviopack.com>
2022-12-14 17:06:27 -0300

    #1 modifique App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import UsuarioContext from './contexts/UsuarioContext';
+import Cocteles from './sections/Cocteles/Cocteles';
+import Pagina404 from './sections/Pagina404/Pagina404';
+import Series from './sections/Series/Series';
+import SobreMi from './sections/SobreMi/SobreMi';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hola soy Milton</h1>
-      <h2>Gera me hace laburar</h2>
-    </div>
-  );
+import './styles.css';
+
+function App() {
+    const userName = 'Milton';
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+    function userLogIn() {
+        setIsUserLoggedIn(true);
+    }
+
+    function userLogOut() {
+        setIsUserLoggedIn(false);
+    }
+
+    return (
+        <UsuarioContext.Provider value={{ userName, isUserLoggedIn, userLogIn, userLogOut }}>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Navigate to="series" replace />} />
+                    <Route path="series" element={<Series />} />
+                    <Route path="cocteles" element={<Cocteles />} />
+                    <Route path="sobremi" element={<SobreMi />} />
+                    <Route path="*" element={<Pagina404 />} />
+                </Routes>
+            </BrowserRouter>
+        </UsuarioContext.Provider>
+    );
 }
+
+export default App;
