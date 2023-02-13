26565e36a212b54636639647d51fe3087c2fdd98
Gerardo Muñoz <gerardo@enviopack.com>
2023-02-13 16:30:46 -0300

    #4 mejoras en el componente y se borra styled.js

--- src/sections/RickAndMorty/RickAndMorty.jsx
+++ src/sections/RickAndMorty/RickAndMorty.jsx
@@ -2,13 +2,10 @@ import { useEffect, useState } from 'react';
 import { Box, Card, Text, Stack, Badge, Skeleton } from '@enviopack/enviopack-ui';
 import Title from '../../components/Title/Title';
 import Image from '../../components/Image/Image';
-//import Card from '../../components/Card/Card';
 import useFetch from '../../hooks/useFetch';
-import * as S from './styled.js';
 
 function RickAndMorty() {
     const [characters, setCharacters] = useState([]);
-    
 
     const [responseRickAndMortyApi, fetchRickAndMortyApi] = useFetch(
         'https://rickandmortyapi.com/api/character'
@@ -25,35 +22,30 @@ function RickAndMorty() {
    }, [responseRickAndMortyApi]);
 
      return (
-        
         <Box>
             <Title>Personajes</Title>
-           {characters.length > 0 ? (
-            <S.ContainerCards>
+            {characters.length > 0 ? (
+            <Box display="flex" flexDirection="row" style={{flexWrap: "wrap"}}>
                 {
                     characters.map((item) => (
-                        <S.ItemCard>
-                            <Card marginBottom="16px" backgroundColor='white-alpha-600' borderRadius="10px" padding="16px" marginTop="8px">
-                              <Title isSmall>{item.name}</Title>
-                              <Image width="200px" isBordered url={item.image} />
-                                <Stack>
-                                    <Text fontWeight="bold" color="red-600" fontSize="md">Estado: {<Badge size="md" color={item.status === 'Alive' ? 'green' : 'red'}>{item.status}</Badge>}</Text>
-                                    <Text fontWeight="bold" color="black-600" fontSize="md">Especie: {item.species}</Text>
-                                </Stack>
-                            </Card>
-                        </S.ItemCard>        
+                        <Card marginBottom="16px" backgroundColor='blue-400' borderRadius="10px" padding="16px" margin="8px" flexGrow="1" width="300px">
+                            <Title isSmall>{item.name}</Title>
+                            <Image width="200px" isBordered url={item.image} />
+                            <Stack>
+                                <Text fontWeight="bold" color="red-600" fontSize="md">Estado: {<Badge size="md" color={item.status === 'Alive' ? 'green' : 'red'}>{item.status}</Badge>}</Text>
+                                <Text fontWeight="bold" color="black-600" fontSize="md">Especie: {item.species}</Text>
+                            </Stack>
+                        </Card>       
                     ))
                 }
-            </S.ContainerCards>
-            ) : (    
+            </Box>
+            ) : (   
             <Stack gap="0.5rem">
                <Skeleton />
                <Skeleton height="10rem" />
             </Stack>
             )}
         </Box>
-
-            
     );
 }
 
